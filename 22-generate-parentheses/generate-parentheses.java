a7ec54c00094ee3d261f5a85d4fff53bc7172f26
DeepakNegi13 <268430169+DeepakNegi13@users.noreply.github.com>
2026-09-16 21:38:27 +0530

    Time: 3 ms (14.18%) | Memory: 45.1 MB (12.23%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    //hint:- at any point of time the number of opening brackit is greater then equal to number of cosing bracket
+    public List<String> generateParenthesis(int n) {
+		List<String> ans = new ArrayList<>();
+		String str = new String();
+		helperString(n,0,0,ans,str);
+		return ans;
+	}
+
+	private void helperString(int n, int open,int close, List<String> ans, String str) {
+		if(open<close || open>n || close>n) return ;
+		if(open==close&&open==n){
+			ans.add(new String(str));
+			return;
+		}
+        helperString(n,open+1,close,ans,str+"(");
+        helperString(n,open,close+1,ans,str+")");
+        
+	}
+}
